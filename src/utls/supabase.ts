8641d7a25ca22b5315f33d5de5be614194c97e07
Jose G <jose.mauricio0226@gmail.com>
2025-05-04 23:08:58 -0400

    Update supabase.ts

--- src/utls/supabase.ts
+++ src/utls/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL || "",
-  process.env.REACT_APP_SUPABASE_ANON_KEY || "",
+  process.env.VITE_SUPABASE_URL || "",
+  process.env.VITE_SUPABASE_ANON_KEY || "",
   {
     auth: {
       storage: localStorage, // Use localStorage for web
